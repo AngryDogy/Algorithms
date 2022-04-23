d6cba19ac153d725dc3b6b6be787378f2967b664
Vladislav <59117547+AngryDogy@users.noreply.github.com>
2022-04-23 16:26:11 +0300

    Update main.cpp

--- Графы/Алгоритм Форд-Белмана/main.cpp
+++ Графы/Алгоритм Форд-Белмана/main.cpp
@@ -1,42 +1,42 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 typedef long long ll;
-const ll inf = 1e9;
-ll n, m, val, q1, q2;
-ll g[500][500], f[500][500];
-
-int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cin >> val;
-            if (val == -1001) {
-                g[i][j] = inf;
-                f[i][j] = inf;
-            } else {
-                g[i][j] = val;
-                f[i][j] = val;
-            }
-        }
-    }
-    for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (f[i][k] != inf && f[k][j] != inf)
-                    f[i][j] = min(f[i][j], f[i][k] + f[k][j]);
-            }
-        }
-    }
-    for (int i = 0; i < m; i++) {
-        cin >> q1 >> q2;
-        if (f[q1][q2] == inf)
-            cout << "No path" << endl;
-        else
-            cout << f[q1][q2] << endl;
-
-    }
-    return 0;
+const ll inf=1e9;
+const ll size=1e3;
+vector<vector<pair<ll, ll>>> g;
+ll n, m, v1, v2, p;
+ll ford[size];
+int main()
+{
+	freopen("shortest.in","r",stdin);
+	freopen("shortest.out","w",stdout);
+	cin>>n>>m;
+	g.resize(n*m+10);
+	fill(ford, ford+n+10, inf);
+	for(int i=0;i<m;i++)
+	{
+		cin>>v1>>v2>>p;
+	        g[v1].push_back(make_pair(v2, p));
+			
+	}
+	ford[1]=0;
+	for(int k=0;k<n;k++)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=0;j<g[i].size(); j++)
+			{
+				if(ford[i]!=inf)
+					ford[g[i][j].first]=min(ford[g[i][j].first], ford[i]+g[i][j].second);	
+			}
+		}
+	}
+	for(int i=1;i<=n;i++)
+	{
+		if(ford[i]==inf)
+			cout<<30000<<' ';
+	        else
+	        	cout<<ford[i]<<' ';
+	}
+	return 0;
 }
